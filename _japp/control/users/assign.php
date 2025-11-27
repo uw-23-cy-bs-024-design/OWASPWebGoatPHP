a4f7084892a220d601ed7cadd072c48c61efa973
uw-23-cy-bs-024-design <uw-23-cy-bs-024@student.uow.edu.pk>
2025-11-27 08:19:19 -0800

    Update assign.php

--- _japp/control/users/assign.php
+++ _japp/control/users/assign.php
@@ -1,31 +1,25 @@
-<?php
 class UsersAssignController extends BaseControllerClass
 {
-	function Start()
-	{
-		$View=$this->View;
-        
-		if (isset($_POST['rid']))
-		{
-		    $Replace=$_POST['Replace'];
-            if ($_POST['rid'])
-		    foreach ($_POST['rid'] as $R)
-            {
-                if ($_POST['uid'])
-                foreach ($_POST['uid'] as $U)
-                {
-                    $this->App->RBAC->User_AssignRole($R,$U,$Replace);
+    function Start()
+    {
+        $View = $this->View;
+
+        if (isset($_POST['rid'])) {
+            $Replace = $_POST['Replace'];
+
+            if (!empty($_POST['rid'])) {
+                foreach ($_POST['rid'] as $R) {
+
+                    if (!empty($_POST['uid'])) {   // ← Added curly braces here
+                        foreach ($_POST['uid'] as $U) {
+                            $this->App->RBAC->User_AssignRole($R, $U, $Replace);
+                        }
+                    }
+
                 }
             }
-            $View->Result=count($_POST['rid'])*count($_POST['uid']);
-		    
-		}
-		
-		$Userman=new SystemUsersModel($this->App);
-		$View->Users=$Userman->AllUsers();
-		$View->Roles=$this->App->RBAC->Role_All();
-		
-		$this->Present();
-	}
+
+            $View->Result = count($_POST['rid']) * count($_POST['uid']);
+        }
+    }
 }
-?>
